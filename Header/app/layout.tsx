be3b77e02365b696b7ce3ccd1ec6e1e5c47edb6f
ElvisKurtbekov <122432639+ElvisKurtbekov@users.noreply.github.com>
2024-11-14 15:36:05 +0300

    Добавление Header

---
+++ Header/app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import {Poppins} from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Evently is a platform for organizing events and activities.",
+  icons: {
+    icon: '/assets/images/logo.svg',
+  }
+};
+
+export default function RootLayout({
+  children,
+}:{
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>        
+        {children}
+      </body>
+    </html>
+  );
+}
+
