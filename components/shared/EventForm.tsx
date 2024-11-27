4f7455e46e1721c5a7d658baaa4c41e607be377a
ElvisKurtbekov <122432639+ElvisKurtbekov@users.noreply.github.com>
2024-11-27 23:04:19 +0300

    Добавлен компонент DatePicker с базовым функционалом

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -35,7 +35,6 @@ type EventFormProps = {
 const EventForm = ({userId, type}: EventFormProps) => {
 
     const [files, setFiles] = useState<File[]>([])
-    const [startDate, setStartDate] = useState(new Date());
     const initialValues = eventDefaultValues;
 
     const { startUpload } = useUploadThing('imageUploader')
@@ -149,15 +148,124 @@ const EventForm = ({userId, type}: EventFormProps) => {
                                 className="filter-grey"
                             />
                             <p className="ml-3 whitespace-nowrap text-grey-600">Дата начала:</p>
+                            <DatePicker 
+                                selected={field.value} 
+                                onChange={(date: Date | null) => field.onChange(date)} 
+                                showTimeSelect
+                                timeInputLabel="Time"
+                                dateFormat="dd.MM.yyyy HH:mm"
+                                timeFormat="HH:mm"
+                                wrapperClassName="datePicker"
+                            />
                         </div>                       
                     </FormControl>
                     <FormMessage />
                     </FormItem>
                 )}
             />            
-          </div>                       
+            <FormField
+                control={form.control}
+                name="endDateTime"
+                render={({ field }) => (
+                    <FormItem className="w-full">
+                    <FormControl>
+                        <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                            <Image
+                                src="/assets/icons/calendar.svg"
+                                alt="calendar"
+                                width={24}
+                                height={24}
+                                className="filter-grey"
+                            />
+                            <p className="ml-3 whitespace-nowrap text-grey-600">Дата окончания:</p>
+                            <DatePicker 
+                                selected={field.value} 
+                                onChange={(date: Date | null) => field.onChange(date)} 
+                                showTimeSelect
+                                timeInputLabel="Time"
+                                dateFormat="dd.MM.yyyy HH:mm"
+                                timeFormat="HH:mm"
+                                wrapperClassName="datePicker"
+                            />
+                        </div>                       
+                    </FormControl>
+                    <FormMessage />
+                    </FormItem>
+                )}
+            />            
+        </div>
 
-        <Button type="submit">Отправить</Button>   {/*на 30 строке надо исправить*/}
+        <div className="flex flex-col gap-5 md:flex-row">
+        <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/dollar.svg"
+                        alt="dollar"
+                        width={24}
+                        height={24}
+                        className="filter-grey"
+                      />
+                      <Input type="number" placeholder="Цена" {...field} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
+                      <FormField
+                        control={form.control}
+                        name="isFree"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <div className="flex items-center">
+                                <label htmlFor="isFree" className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Бесплатный билет</label>
+                                <Checkbox
+                                  id="isFree" className="mr-2 h-5 w-5 border-2 border-primary-500"
+                                   />
+                              </div>
+          
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />   
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            /> 
+            <FormField
+              control={form.control}
+              name="url"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormControl>
+                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                      <Image
+                        src="/assets/icons/link.svg"
+                        alt="link"
+                        width={24}
+                        height={24}
+                      />
+                      <Input placeholder="URL" {...field} className="input-field" />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+        </div>
+        
+        <Button 
+          type="submit"
+          size="lg"
+          disabled={form.formState.isSubmitting}
+          className="button col-span-2 w-full"
+        >
+          {form.formState.isSubmitting ? (
+            'Submitting...'
+          ): `${type} Мероприятие `}</Button>   {/*на 30 строке надо исправить*/}
       </form>
     </Form>
   )
