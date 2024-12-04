9b297202ac5a42aa23108f53c6e4415468f633b6
AlexeyZhuravko <zhuravko1778@mail.ru>
2024-12-04 21:42:58 +0300

    Добавление функционала обновления и редактирования событий

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -17,6 +17,10 @@ import {
   GetRelatedEventsByCategoryParams,
 } from '@/types'
 
+const getCategoryByName = async (name: string) => {
+  return Category.findOne({ name: { $regex: name, $options: 'i' } })
+}
+
 const populateEvent = (query: any) => {
   return query
     .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
@@ -50,6 +54,29 @@ export async function getEventById(eventId: string) {
     if (!event) throw new Error('Event not found')
 
     return JSON.parse(JSON.stringify(event))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// UPDATE
+export async function updateEvent({ userId, event, path }: UpdateEventParams) {
+  try {
+    await connectToDatabase()
+
+    const eventToUpdate = await Event.findById(event._id)
+    if (!eventToUpdate || eventToUpdate.organizer.toHexString() !== userId) {
+      throw new Error('Unauthorized or event not found')
+    }
+
+    const updatedEvent = await Event.findByIdAndUpdate(
+      event._id,
+      { ...event, category: event.categoryId },
+      { new: true }
+    )
+    revalidatePath(path)
+
+    return JSON.parse(JSON.stringify(updatedEvent))
   } catch (error) {
     handleError(error)
   }
