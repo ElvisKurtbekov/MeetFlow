7fa3bc309e2533daf03baf6498ea2b011e85934f
ElvisKurtbekov <122432639+ElvisKurtbekov@users.noreply.github.com>
2024-12-08 16:46:38 +0300

    Добавление функции фильтрации по категориям

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image 
-            src = "/assets/images/logo.svg" width={128} height={38} 
-            alt="Evently Logo" 
+            src = "/assets/images/logo2.svg" width={128} height={38} 
+            alt="Meetflow Logo" 
           />
         </Link>
 
